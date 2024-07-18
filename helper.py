3c2bdda49ef8bc4517d35fa8317ad18347b6a80e
TheFallenToast3r <unknown8923098@gmail.com>
2024-07-18 14:33:16 -0700

    creating handling functions

---
+++ helper.py
@@ -0,0 +1,46 @@
+from Crypto.Cipher import Salsa20
+from Crypto.Random import get_random_bytes
+import pandas as pd
+from io import StringIO
+import hashlib
+from Crypto.Hash import HMAC, SHA256
+
+
+def write_encrypted_file(df, secret_key, file_name):
+    with open('encryptedinfo/' + file_name, 'wb') as f:
+        cipher = Salsa20.new(key=secret_key)
+        msg = cipher.nonce + cipher.encrypt(df.to_csv().encode('utf-8'))
+        f.write(msg)
+        print(f"csv; {df.to_csv()}")
+        print(f"encoded: {df.to_csv().encode('utf-8')}")
+        f.close()
+
+
+def return_decrypted_dataframe(file_name, secret_key):
+    with open('encryptedinfo/' + file_name, 'rb') as f:
+        message = f.read()
+        msg_nonce = message[:8]
+        ciphertext = message[8:]
+        cipher = Salsa20.new(key=secret_key, nonce=msg_nonce)
+        plaintext1 = cipher.decrypt(ciphertext)
+        ptext = plaintext1.decode('utf-8')
+        f.close()
+        return pd.read_csv(StringIO(ptext))
+
+def generate_hash(msg, secret_key):
+    h = HMAC.new(secret_key, digestmod=SHA256)
+    h.update(msg)
+    # print(h.hexdigest())
+    return h.hexdigest()
+
+
+def verify_hash(msg, secret_key, mac_hash):
+    h = HMAC.new(secret_key, digestmod=SHA256)
+    h.update(msg)
+    try:
+        h.hexverify(mac_hash)
+        print("The message '%s' is authentic" % msg)
+        return True
+    except ValueError:
+        print("The message or the key is wrong")
+        return False
--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
+from Crypto.Cipher import Salsa20
 import pandas as pd
 import hashlib
+from helper import return_decrypted_dataframe, write_encrypted_file, generate_hash, verify_hash
+from Crypto.Hash import HMAC, SHA256
 
 
 df = pd.DataFrame(pd.read_csv('encryptedinfo/pswd.csv'))
-# USR,EMAIL,PSWD,URL,TAGS,ROWHASH
 user = "user"
 email = "email@example.com"
 password = "password"
 URL = "https://example.com/login"
 TAGS = "[Banking, Gaming]"
-salt = "salt"
-stringex = (user + email + password + URL + TAGS + salt).encode(encoding="utf-8")
-print(stringex)
-hash = hashlib.sha256()
-hash.update(stringex)
-new_row = {'USR': user, 'EMAIL': email, 'PSWD': password, 'URL': URL, 'TAGS': TAGS, 'SALT': "salt"}
 
-df.loc[len(df)] = new_row
-df.to_csv('encryptedinfo/pswd.csv', index=False)
+# stringex = (user + email + password + URL + TAGS + salt).encode(encoding="utf-8")
+# print(stringex)
+# hash = hashlib.sha256()
+# hash.update(stringex)
+new_row = {'USR': user, 'EMAIL': email, 'PSWD': password, 'URL': URL, 'TAGS': TAGS}
+
+# df.loc[len(df)] = new_row
+# df.to_csv('encryptedinfo/pswd.csv', index=False)
+
 
 # df = pd.DataFrame(pd.read_csv('encryptedinfo/pswd.csv'))
-# print(df)
+secret_key = b"*Thirty-two byte (256 bits) key*"
+# write_encrypted_file(df, secret_key, "encrypted.bin")
 
-m = hashlib.sha256()
-m.update(b"a")
-print(m.hexdigest())
+# print(return_decrypted_dataframe("encrypted.bin", secret_key))
+msg = df.to_csv().encode('utf-8')
+hash_ = generate_hash(msg, secret_key)
+
+verify_hash(msg, secret_key, hash_)
 
 
 
-obj = AES.new(b'This is a key123', AES.MODE_CBC, b'This is an IV456')
-message = b"The answer is no"
-ciphertext = obj.encrypt(message)
-print(ciphertext)
-obj2 = AES.new(b'This is a key123', AES.MODE_CBC, b'This is an IV456')
-print(obj2.decrypt(ciphertext))
+
